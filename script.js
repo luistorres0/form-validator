71e756363d2562370f8a0e21eddeb19cb1e7d5bf
Luis Torres <luisdtor@gmail.com>
2021-08-10 18:34:06 -0500

    Added validation for 'username' field

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+function showSuccress(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
